711af517057729b06d750198d089e170415805a1
Sir Flumphy <ioan.alex.s@gmail.com>
2023-03-17 15:15:20 +0000

    Bug fixed moon spawning and made the planet visualization more efficient and simpler for less abled programmers to use.

--- Planet.py
+++ Planet.py
@@ -21,23 +21,27 @@ class Planet:
         sun = plt.Circle((self.xpos, self.ypos), self.radius, color='yellow', fill=True, label='Sun')
         plt.gca().add_artist(sun)
 
-    def spawn_moons(self, total):
+    def spawn_moons(self, total, other):
         self.planets = []
         for i in range(total):
             r = self.radius * 0.5
             #d = np.random.uniform(100, 200)
-            #xpos = np.random.randint(100, 200)
-            #ypos = np.random.randint(100, 200)
-            xpos = 0
-            ypos = np.random.randint(100, 300)
-            self.planets.append(Planet(xpos, ypos, 0, 0, 0, r))
+            xpos = np.random.randint(-100, 200)
+            ypos = np.random.randint(-100, 200)
+            #xpos = 0
+            #ypos = np.random.randint(-100, 100)
+            self.planets.append(Planet(other.xpos+xpos, other.ypos+ypos, 0, 0, 0, r)) #Need to add that it's xpos and ypos from the planet
         return self.planets
+
+    def show_moon(self):
+        moon = plt.Circle((self.xpos, self.ypos), self.radius, color='blue', fill=True)
+        plt.gca().add_artist(moon)
 
     def show_planet(self):
         celestial_body = plt.Circle((self.xpos, self.ypos), self.radius, color='white', fill=True, label='Planet')
         plt.gca().add_artist(celestial_body)
+        #plt.plot(self.x_array, self.y_array, color = 'white', linestyle = '--')
 
         if self.planets is not None:
             for planet in self.planets:
-                planet.show_planet()
-        #plt.plot(self.x_array, self.y_array, color = 'white', linestyle = '--')
+                planet.show_moon()
--- main.py
+++ main.py
@@ -16,35 +16,49 @@ for i, row in enumerate(data_list):
 #planet_0.show_sun()
 #planet_1.show_planet()
 
-visualize_planets_names = ['sun', 'mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
-visualize_planets_xpos = [0, 50, 75, 100, 150, 250, 305, 360, 420, 480]
-visualize_planets_ypos = [0, 50, 0, -100, 0, -250, 0, -360, 0, -480]
-visualize_planets_xvel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-visualize_planets_yvel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-visualize_planets_mass = [1000, 20, 40, 50, 55, 85, 65, 75, 80, 10]
-visualize_planets_radius = [25, 5, 5, 7, 7, 20, 5, 5, 15, 2]
+#Initial Planet Visualization Arrays to prop the simulation all I took out was sun, 0, 0, 0, 0, 1000, 25, 0
+#visualize_planets_names = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
+#visualize_planets_xpos = [50, 75, 100, 150, 250, 305, 360, 420, 480]
+#visualize_planets_ypos = [50, 0, -100, 0, -250, 0, -360, 0, -480]
+#visualize_planets_xvel = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#visualize_planets_yvel = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#visualize_planets_mass = [20, 40, 50, 55, 85, 65, 75, 80, 10] #1000
+#visualize_planets_radius = [5, 5, 7, 7, 20, 5, 5, 15, 2] #25
+#visualize_planets_moons = [0, 0, 1, 2, 9, 8, 6, 5, 3]
 
 
+#Bad Practice but archived for future use.
+#for i in range(len(visualize_planets_names)):
+#    visualize_planets_names[i] = p.Planet(visualize_planets_xpos[i], visualize_planets_ypos[i], visualize_planets_xvel[i], visualize_planets_yvel[i], visualize_planets_mass[i], visualize_planets_radius[i])
 
-for i in range(len(visualize_planets_names)):
-    visualize_planets_names[i] = p.Planet(visualize_planets_xpos[i], visualize_planets_ypos[i], visualize_planets_xvel[i], visualize_planets_yvel[i], visualize_planets_mass[i], visualize_planets_radius[i])
+#Creating the planet objects and their number of moons
+#object = p.Planet(xpos, ypos, xvel, yvel, mass, radius)
+visual_sun = p.Planet(0, 0, 0, 0, 500, 10)
+visual_mercury = p.Planet(150, 0, 5, 10, 100, 5)
+visual_venus = p.Planet(250, 0, 10, 15, 110, 6)
+visual_earth = p.Planet(300, 125, 20, 25, 140, 8)
+visual_moon_array = [0, 2, 1]
 
-planet = p.Planet(150, 0, 5, 10, 100, 5)
-planet2 = p.Planet(0, 0, 0, 0, 500, 10)
+#Complete Planet Visualization Array
+visual_planets_array = [visual_mercury, visual_venus, visual_earth]
 
 plt.figure(figsize=(6, 6))
-plt.axis([-500, 500, -500, 500])
+plt.axis([-1000, 1000, -1000, 1000])
 plt.gca().set_facecolor((0, 0, 0))
 plt.title('Simulation in Space')
 plt.xlabel('Displacement')
 plt.ylabel('Displacement')
 plt.gca().set_aspect('equal', adjustable='box')
 
-planet.spawn_moons(3)
-planet.show_planet()
-planet2.show_planet()
+for n in range(len(visual_planets_array)):
+    visual_planets_array[n].spawn_moons(n, visual_planets_array[n])
+    visual_planets_array[n].show_planet()
 
+
+#Using Dynamic Variable Naming, Bad Practice
+#visual_sun.show_sun()
 #for j in range(len(visualize_planets_names)):
+#    visualize_planets_names[j].spawn_moons(visualize_planets_moons[j], visualize_planets_names[j])
 #    visualize_planets_names[j].show_planet()
 
 plt.legend()
