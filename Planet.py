90438e9b9ef7836b37adcd7d29e1153b9abbcbf9
Agustin3107 <82035685+Agustin3107@users.noreply.github.com>
2023-05-02 22:47:08 +0100

    Add files via upload

--- Planet.py
+++ Planet.py
@@ -19,8 +19,16 @@ class Planet:
     DT = 3600
     SCALE = 250 / AU    
     
-    def __init__(self, xpos, ypos, zpos, xvel, yvel, zvel, mass, r, colour, vis_r):
-        #self.name = name
+    def __init__(self, xpos, ypos, zpos, xvel, yvel, zvel, mass,name):
+        
+        self.initial_xpos = xpos
+        self.initial_ypos = ypos
+        self.initial_zpos = zpos
+        self.initial_xvel = xvel
+        self.initial_yvel = yvel
+        self.initial_zvel = zvel
+        
+        self.name = name
         self.xpos = xpos
         self.ypos = ypos
         self.zpos = zpos
@@ -28,19 +36,18 @@ class Planet:
         self.yvel = yvel
         self.zvel = zvel
         self.mass = mass
-        self.radius = r
         self.planets = []
         self.orbit = []
         self.orbitx = []
         self.orbity = []
         self.orbitz = []
-        self.colour = colour
-        self.vis_r = vis_r #Radius when we plot so it looks nice :D
+    
+        self.potential = []
         self.ke = []
         self.gpe = []
 
     def show_properties(self):
-        return self.xpos, self.ypos, self.xvel, self.yvel, self.mass, self.radius
+        return self.xpos, self.ypos, self.xvel, self.yvel, self.mass
 
     def force_calculation(self, other):
         
@@ -55,9 +62,9 @@ class Planet:
         fy = ft * ydist / dist
         fz = ft * zdist / dist
         
-        return fx, fy, fz
+        return fx, fy, fz, dist
     
-    def return_acceleration(self,planet,xpos,ypos,zpos,planets): 
+    def return_acceleration(self,planet,xpos,ypos,zpos,planets,gpe_check): 
         xforce = 0
         yforce = 0
         zforce = 0
@@ -84,26 +91,36 @@ class Planet:
                 ay = yforce/self.mass
                 az = zforce/self.mass
                 
-                potential += -G * otherPlanet.mass * self.mass / dist # calculates the GPE for use in energy conservation plots later
-        self.gpe.append(potential)
+                potential += -G * otherPlanet.mass * self.mass / dist # calculates the GPE for use in energy conservation plots 
+        if gpe_check == True:    
+            self.gpe.append(potential/2)
         return np.array([ax,ay,az]) # returns a as a vector
     
-    def update_planet_position(self,dt, planets):
+    def update_planet_position_euler(self, planets, dt):
         fxt = fyt = fzt = 0
-        dt = int(dt)
+        potential = 0
+
+        #Making sure the planet we're calculating the gravitational forces for is not with itself.
         for planet in planets:
             if self == planet:
                 continue
 
-            fx, fy, fz = self.force_calculation(planet)
+            #Finding the forces and acceleration of the planet.
+            fx, fy, fz, dist = self.force_calculation(planet)
             fxt += fx
             fyt += fy
             fzt += fz
 
+            #Finding the velocity from F = ma
             ax = fxt / self.mass
             ay = fyt / self.mass
             az = fzt / self.mass
-            
+
+            potential += -G * self.mass * planet.mass / dist
+
+        self.gpe.append(potential/2)
+
+        #Updating velocities and position of planet.
         self.xvel += ax * dt
         self.yvel += ay * dt
         self.zvel += az * dt
@@ -111,37 +128,81 @@ class Planet:
         self.ypos += self.yvel * dt
         self.zpos += self.zvel * dt
 
+        #Appending the planet's orbit to arrays
         self.orbit.append((self.xpos, self.ypos, self.zpos))
         self.orbitx.append(self.xpos)
         self.orbity.append(self.ypos)
         self.orbitz.append(self.zpos)
-        
-    def update_RK4(self,dt,planets): 
-        dt = float(dt)
-        totalke_temp = 0
-        totalgpe_temp = 0 # temporary Ke, GPE variables for use in a loop
-        
+
+        self.ke.append(0.5 * self.mass * (self.xvel**2 + self.yvel**2 + self.zvel**2))
+    
+    def update_planet_position_verlet(self, planets, dt):
+        #Setting the initial acceleration as 0
+        ax = ay = az = 0
+        #Making sure the 2 planets we're working with aren't the same
+        potential = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            #Calculating the forces of the planets
+            fx, fy, fz, dist = self.force_calculation(planet)
+            ax += fx / self.mass
+            ay += fy / self.mass
+            az += fz / self.mass
+            potential += -G * self.mass * planet.mass / dist
+
+        #Updating position
+        self.xpos += self.xvel * dt + 0.5 * ax * dt ** 2
+        self.ypos += self.yvel * dt + 0.5 * ay * dt ** 2
+        self.zpos += self.zvel * dt + 0.5 * az * dt ** 2
+
+        ax_new = ay_new = az_new = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy, fz, dist = self.force_calculation(planet)
+            ax_new += fx / self.mass
+            ay_new += fy / self.mass
+            az_new += fz / self.mass
+
+        self.gpe.append(np.sum(potential)/2)
+
+        #Updating new velocities using the average of the start acceleration and next acceleration
+        self.xvel += 0.5 * (ax + ax_new) * dt
+        self.yvel += 0.5 * (ay + ay_new) * dt
+        self.zvel += 0.5 * (az + az_new) * dt
+
+        self.orbit.append((self.xpos, self.ypos))
+        self.orbitx.append(self.xpos)
+        self.orbity.append(self.ypos)
+        self.orbitz.append(self.zpos)
+
+        self.ke.append(0.5 * self.mass * (self.xvel ** 2 + self.yvel ** 2 + self.zvel ** 2))
+    
+    def update_planet_position_rk4(self,planets,dt):      
         vel = np.array([self.xvel , self.yvel, self.zvel])
         initialPos = np.array([self.xpos,self.ypos, self.zpos]) # initial velocity and position vectors for use in the steps
         
         # first step in RK4 uses the initial values
-        dv1 = dt * self.return_acceleration(self , initialPos[0] , initialPos[1], initialPos[2],planets)
+        dv1 = dt * self.return_acceleration(self , initialPos[0] , initialPos[1], initialPos[2],planets,False)
         dr1 = dt * vel
         
         # second step in RK4 , takes a dt/2 step forward
-        dr2 = dt * (vel + self.return_acceleration(self , initialPos[0] , initialPos[1], initialPos[2],planets) * dt/2)
+        dr2 = dt * (vel + self.return_acceleration(self , initialPos[0] , initialPos[1], initialPos[2],planets,False) * dt/2)
         r = initialPos + (dr1/2)
-        dv2 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets) #uses the new r to get the next velocity
+        dv2 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets,False) #uses the new r to get the next velocity
         
         # third step in RK4 , takes another dt/2 step like step 2 but instead feeds back step 2's velocity and position for this iteration
         dr3 = dt * (vel + dv2/2)
         r = initialPos + (dr2/2) #note r here is not the r from step 2 adding on dr2/2 but instead the initial position as demanded by RK4 method
-        dv3 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets)
+        dv3 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets,False)
         
         # fourth step , takes a full step in dt and uses the third steps values
         dr4 = dt * (vel + dv3)
         r = initialPos + (dr3)
-        dv4 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets)
+        dv4 = dt * self.return_acceleration(self , r[0] , r[1], r[2],planets,False)
         
         finalv = vel + (1/6) * (dv1 + 2*dv2 + 2*dv3 + dv4) # changes the velocity and position by the coefficients needed for RK4
         finalpos = initialPos + (1/6) * (dr1 + 2*dr2 + 2*dr3 + dr4)
@@ -151,8 +212,7 @@ class Planet:
         self.zvel = finalv[2] # assigns the new velocity to the planet
         
         self.ke.append(0.5 * self.mass * (self.xvel**2 + self.yvel**2 + self.zvel**2))
-        totalke_temp = totalke_temp + 0.5 * self.mass * (self.xvel**2 + self.yvel**2 + self.zvel**2) #calculates the Ke and adds it to the object and also calculates the total systems Ke
-        
+        self.return_acceleration(self,finalpos[0],finalpos[1],finalpos[2],planets,True)
         self.xpos = finalpos[0]
         self.ypos = finalpos[1]
         self.zpos = finalpos[2] # assigns new coords to the planet
@@ -161,11 +221,10 @@ class Planet:
         self.orbitx.append(self.xpos)
         self.orbity.append(self.ypos)
         self.orbitz.append(self.zpos)
-        totalgpe_temp += self.gpe[-1] # sums the GPE to the total system gpe. GPE is summed twice so is divided by 2 later
+        
         
         #remember to do the total ke and gpe in the other parts of the code
         
-    
     def show_sun(self):
         sun = plt.Circle((self.xpos*self.SCALE, self.ypos*self.SCALE), self.radius*self.SCALE, color='yellow', fill=True, label='Sun')
         plt.gca().add_artist(sun)
